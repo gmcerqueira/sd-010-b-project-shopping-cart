171f3a4b9d0f08fda65aab887ad3ea422966d783
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-04-14 18:44:24 -0300

    implemented fetch and render item list functions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,6 +12,24 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+const getSiblingThatContainsID = (element) => {
+  const { children } = element.parentElement;
+  let foundElement = '';
+  Array.from(children).forEach((child) => {
+    const classOfElement = child.className;
+    if (classOfElement === 'item__sku') {
+      foundElement = child.innerText;
+    } 
+  });
+  return foundElement;
+};
+
+const renderedBtnListener = async (e) => {
+  const id = getSiblingThatContainsID(e.target);
+  const res = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  console.log(await res.json());
+};
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -21,7 +37,9 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const renderedItemButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  renderedItemButton.addEventListener('click', renderedBtnListener);
+  section.appendChild(renderedItemButton);
 
   return section;
 }
@@ -31,7 +49,6 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -41,3 +58,26 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const renderResults = (items) => {
+  const itemContainer = document.querySelector('.items');
+  items.forEach(({ id, title, thumbnail }) => {
+    const params = {
+      sku: id,
+      name: title,
+      image: thumbnail,
+    };
+    const element = createProductItemElement(params);
+    itemContainer.appendChild(element);
+  });
+};
+
+const getResults = async () => {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=Monitor');
+  const { results } = await response.json();
+  renderResults(results);
+};
+
+window.onload = function onload() {
+  getResults();
+};
