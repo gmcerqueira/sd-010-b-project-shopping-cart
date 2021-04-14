934a77fefb2e3eb8f979da20283b35e32a86c143
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-04-14 16:39:26 -0300

    Resolvendo requisito 1 do projeto

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -13,7 +11,7 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
- 
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -26,18 +24,38 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+function fetchApi($QUERY) {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${$QUERY}`;
+
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.forEach((result) => {
+        const newObj = {
+          sku: result.id, name: result.title, image: result.thumbnail,
+        };
+        const itemsClass = document.querySelector('.items');
+        itemsClass.appendChild(createProductItemElement(newObj));
+      });
+    });
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+window.onload = function onload() {
+  fetchApi('computador');
+};
