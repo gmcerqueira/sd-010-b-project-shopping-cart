91414b95708b65b6da6468e2ba777030334d83d1
Fabio Marturano <fabiocmjor@gmail.com>
2021-04-14 18:45:20 -0300

    Ajustes Lint

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 window.onload = function onload() { };
 
+// URL's
+const urlProducts = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+const urlProduct = 'https://api.mercadolibre.com/items/';
+
+const fetchProducts = (item, Url) => fetch(`${Url}${item}`).then((resp) => resp.json());
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,6 +20,35 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function dellCartsSaveds() {
+  localStorage.removeItem('CartItemsToSave');
+}
+
+function saveCart() {
+  const itemsToSave = [];
+  dellCartsSaveds();
+  const allItemsNow = document.querySelectorAll('.cart__item');
+  allItemsNow.forEach((item) => {
+    const itemObj = { item: item.innerText };
+    itemsToSave.push(itemObj);
+  });
+  console.log(itemsToSave);
+}
+
+function cartItemClickListener(event) {
+  const cardItems = document.querySelector('.cart__items');
+  cardItems.removeChild(event.srcElement);
+  saveCart();
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener); 
+  return li;
+}
+
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -21,10 +56,12 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  const button = section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  button.addEventListener('click', async () => {
+  const buttonAdd = section
+    .appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  buttonAdd.addEventListener('click', async () => {
     const cardItems = document.querySelector('.cart__items');
-    const idItem = button.parentNode.firstChild.innerText;
+    const idItem = buttonAdd.parentNode.firstChild.innerText;
     const itemInfo = await fetchProducts(idItem, urlProduct);
     cardItems.appendChild(createCartItemElement(itemInfo));
     saveCart();
@@ -36,25 +73,6 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
-
-function cartItemClickListener(event) {
-  const cardItems = document.querySelector('.cart__items');
-  cardItems.removeChild(event.srcElement);
-}
-
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener); 
-  return li;
-}
-
-// URL's
-const urlProducts = 'https://api.mercadolibre.com/sites/MLB/search?q=';
-const urlProduct = 'https://api.mercadolibre.com/items/';
-
-const fetchProducts = (item, Url) => fetch(`${Url}${item}`).then((resp) => resp.json());
 
 const mountItems = async (product) => {  
   const conteinerItems = document.querySelector('.items');
@@ -74,18 +92,3 @@ const mountItems = async (product) => {
 
 mountItems('computador');
 // setTimeout(() => reviveButtons(), 500);
-
-function dellCartsSaveds() {
-  localStorage.removeItem('CartItemsToSave');
-}
-
-function saveCart(){
-  const itemsToSave = [];
-  dellCartsSaveds();
-  const allItemsNow = document.querySelectorAll('.cart__item');
-  allItemsNow.forEach((item) => {
-    const itemObj = { item: item.innerText };
-    itemsToSave.push(itemObj);
-  });
-  console.log(itemsToSave);
-}
