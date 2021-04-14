57476413cb147117ea91c27f12fc39a7a3ba191d
Jessie <miss_sc_jessi@hotmail.com>
2021-04-14 16:08:11 -0300

    requisito 1 lint agr vai

--- script.js
+++ script.js
@@ -1,67 +1,65 @@
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createProductImageElement(imageSource) {
-  //   const img = document.createElement("img");
-  //   img.className = "item__image";
-  //   img.src = imageSource;
-  //   return img;
-  // }
-  
-  // function createCustomElement(element, className, innerText) {
-    //   const e = document.createElement(element);
-    //   e.className = className;
-    //   e.innerText = innerText;
-    //   return e;
-    // }
-    
-    function createProductItemElement(sku, name, image) {
-      const section = document.createElement('section');
-      section.className = 'item';
-      
-      section.appendChild(createCustomElement('span', 'item__sku', sku));
-      section.appendChild(createCustomElement('span', 'item__title', name));
-      section.appendChild(createProductImageElement(image));
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
+function createProductItemElement(sku, name, image) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
   section.appendChild(
-    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')
-    );
-    
-    return section;
-  }
-  
-  // function getSkuFromProductItem(item) {
-    //   return item.querySelector('span.item__sku').innerText;
-    // }
-    
-    // function cartItemClickListener(event) {
-      //   // coloque seu código aqui
-      // }
-      
-      // function createCartItemElement({ sku, name, salePrice }) {
-        //   const li = document.createElement('li');
-        //   li.className = 'cart__item';
-        //   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-        //   li.addEventListener('click', cartItemClickListener);
-        //   return li;
-        // }
-        
-        const api = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-        
-        const listItem = () => {
-          const myObject = {
-            method: 'GET',
-            headers: { Accept: 'application/json' },
-          };
-          fetch(api, myObject)
-          .then((response) => response.json())
-          .then((data) => {
-            data.results.map((element) => {
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
+
+  return section;
+}
+
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+const api = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+const listItem = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+  };
+  fetch(api, myObject)
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.map((element) => {
         const { id, title, thumbnail } = element;
         const sectionPai = document.querySelector('.items');
-        sectionPai.appendChild(createProductItemElement(id, title, thumbnail));
+        return sectionPai.appendChild(createProductItemElement(id, title, thumbnail));
       });
     });
-  };
-  
-  window.onload = function onload() {
-    listItem();
-  };
-  
+};
+
+window.onload = function onload() {
+  listItem();
+};
