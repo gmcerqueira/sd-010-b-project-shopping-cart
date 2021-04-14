1a57cf4df14e56d42c34e6b48b769acdec77476f
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-04-14 18:32:08 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
+  const img = document.createElement("img");
+  img.className = "item__image";
   img.src = imageSource;
   return img;
 }
+
+const recoverProduct = async () => {
+  const items = document.querySelector(".items");
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$computador`).then(
+    (response) => {
+      response.json().then((result) => {
+        const resultado = result.results;
+        console.log(result.results[0]);
+        resultado.forEach((element) => {
+          items.appendChild(createProductItemElement(element));
+        });
+      });
+    }
+  );
+};
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-} 
+}
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const section = document.createElement("section");
+  section.className = "item";
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement("span", "item__sku", sku));
+  section.appendChild(createCustomElement("span", "item__title", name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement("button", "item__add", "Adicionar ao carrinho!")
+  );
 
   return section;
 }
-
+/*
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -41,3 +56,8 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+*/
+
+window.onload = function onload() {
+  recoverProduct();
+};
