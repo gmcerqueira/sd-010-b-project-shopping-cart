1d881e58bf34e80936d176ec02088e5f2367a3e5
bidaffonso <bidaffondo@gmail.com>
2021-04-14 18:02:17 -0400

    Requisito 1 resolvido

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -25,19 +23,39 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
+// const fetch = require('node-fetch');
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+const fetchFunction = (QUERY) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`)
+    .then((response) => response.json())
+      .then((products) => {
+        products.results.forEach((product) => {
+          const item = {
+            sku: product.id,
+            name: product.title,
+            image: product.thumbnail,
+          };
+          document.querySelector('.items').appendChild(createProductItemElement(item));
+        });
+      });
+};
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+window.onload = function onload() {
+  fetchFunction('computador');
+ };
