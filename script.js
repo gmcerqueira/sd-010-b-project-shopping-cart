6b3e8ed47490fb3b5aafdcb10c2c26274ed65700
Mariana Mohr <marianamohr@gmail.com>
2021-04-14 16:27:56 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function onload() { };
-
+// mariana
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
