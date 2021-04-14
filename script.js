2138c7d0c0bd5c3bfb8e731f74de1beca91f4a89
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-04-14 20:27:47 -0300

    Desafio 2 - Colocando coisas no carrinho

--- script.js
+++ script.js
@@ -12,6 +12,23 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener() {
+}
+
+function createCartItemElement({
+  id: sku,
+  title: name,
+  price: salePrice,
+}) {
+  const ol = document.querySelector('ol');
+  const li = document.createElement('li');
+  ol.appendChild(li);
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener());
+  return li;
+}
+
 function createProductItemElement({
   id: sku,
   title: name,
@@ -20,12 +37,16 @@ function createProductItemElement({
   const section = document.createElement('section');
   section.className = 'item';
   const sectionItems = document.querySelector('.items');
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', () => {
+      const API_ITENS = `https://api.mercadolibre.com/items/${sku}`;
+      fetch(API_ITENS)
+        .then((response) => response.json())
+        .then((teste) => createCartItemElement(teste));
+    });
   return sectionItems.appendChild(section);
 }
 
@@ -33,21 +54,15 @@ function createProductItemElement({
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({
-//   sku,
-//   name,
-//   salePrice,
-// }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+window.onload = function onload() {
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.forEach((element) =>
+        createProductItemElement(element));
+    });
+};
 // 1° Desafio
 // Baby Steps: 
 // 1° informação
@@ -58,14 +73,9 @@ function createProductItemElement({
 // 2.2 acionar o createProductItemElement
 // 2.3 adicionar um destructuring no parametro do creatProductItemElement
 // 2.4 colocar dentor do create o 'element' contendo as informações da API
-// 3° o Parametro da API está desestruturado e fará a organização da pesquisa
-// entrega-los para o crateProduction
-window.onload = function onload() {
-  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-  fetch(API_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      data.results.forEach((element) =>
-        createProductItemElement(element));
-    });
-};
+// 3° o Parametro da API está desestruturado e fará a organização da pesquisa entrega-los para o crateProduction.
+// 2°Desafio
+// Baby Steps:
+// 1° Criar evento no botão 'Adicionar ao carrinho" x
+// 2° acessar as informações da API
+// 2.1 
