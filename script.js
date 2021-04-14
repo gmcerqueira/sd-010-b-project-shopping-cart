dfc3c59adbd1434e051592997d3eac5e43b1591e
isTorres <ismaeltorres612@gmail.com>
2021-04-14 18:08:51 -0300

    refatorar

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
-
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
-
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function computerInfo(product) {
-  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
-  .then((response) => response.json())
-  .then((computers) => {
-    computers.results.forEach((element) => {
-      const pc = {
-        sku: element.id,
-        name = element.title,
-        image = element.thumbnail,
-      };
-      document.querySelector('.items').appendChild(createProductItemElement(pc));
-    });
-  });
-  // .catch((_error) => {
-  //   alert('Não foi possível encontrar este produto');
-  // })
-}
-
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
+// function createProductImageElement(imageSource) {
+//   const img = document.createElement('img');
+//   img.className = 'item__image';
+//   img.src = imageSource;
+//   return img;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
+// function createCustomElement(element, className, innerText) {
+//   const e = document.createElement(element);
+//   e.className = className;
+//   e.innerText = innerText;
+//   return e;
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
+// function createProductItemElement({ sku, name, image }) {
+//   const section = document.createElement('section');
+//   section.className = 'item';
+
+//   section.appendChild(createCustomElement('span', 'item__sku', sku));
+//   section.appendChild(createCustomElement('span', 'item__title', name));
+//   section.appendChild(createProductImageElement(image));
+//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+//   return section;
 // }
 
-window.onload = function onload() { 
-  computerInfo('computador');
-};
+// function computerInfo(product) {
+//   return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+//   .then((response) => response.json())
+//   .then((computers) => {
+//     computers.results.forEach((element) => {
+//       const pc = {
+//         sku: element.id,
+//         name: element.title,
+//         image: element.thumbnail,
+//       };
+//       document.querySelector('.items').appendChild(createProductItemElement(pc));
+//     });
+//   });
+//   // .catch((_error) => {
+//   //   alert('Não foi possível encontrar este produto');
+//   // })
+// }
+
+// // function getSkuFromProductItem(item) {
+// //   return item.querySelector('span.item__sku').innerText;
+// // }
+
+// // function cartItemClickListener(event) {
+// //   // coloque seu código aqui
+// // }
+
+// // function createCartItemElement({ sku, name, salePrice }) {
+// //   const li = document.createElement('li');
+// //   li.className = 'cart__item';
+// //   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+// //   li.addEventListener('click', cartItemClickListener);
+// //   return li;
+// // }
+
+// window.onload = function onload() { 
+//   computerInfo('computador');
+// };
