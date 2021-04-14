3aab34b5a0186ce67f33e4f22a85910295913c7f
André Arnoni <andre_arnoni@yahoo.com.br>
2021-04-14 16:12:41 -0300

    executado o primeiro requisito

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ sku = 'id', name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +24,32 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+async function createListOfProducts() {
+  const api = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const { results } = await api.json();
+  results.forEach((product) => {
+    const parentElement = document.getElementsByClassName('items');
+    const creatingItem = createProductItemElement(product);
+    parentElement.appendChild(creatingItem);
+  });
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+window.onload = function onload() {
+  createListOfProducts();
+};
