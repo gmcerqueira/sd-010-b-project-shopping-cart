97d678aeac6c505f97281a28fe4b2fe82c971936
Daniel <dsouzaborini@gmail.com>
2021-04-14 18:53:28 -0300

    corrige função utilizada no requisito 1

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function createCartItemElement({ sku, name, salePrice }) {
 function renderList(products) {
   const sectionList = document.getElementsByClassName('items');
   // const computers = getProducts("computador");
-  products.forEach(({ id, title, price }) => {
-    const elementoLista = createCartItemElement({ sku: id, name: title, salePrice: price });
+  products.forEach(({ id, title, thumbnail }) => {
+    const elementoLista = createProductItemElement({ sku: id, name: title, image: thumbnail });
     sectionList[0].appendChild(elementoLista);
   });
 }
@@ -31,31 +31,31 @@ window.onload = async function onload() {
   renderList(computers);
 };
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 // mod
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   return section;
-// }
+  return section;
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
