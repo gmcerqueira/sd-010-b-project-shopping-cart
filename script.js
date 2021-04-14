847855da6a06720340780c191473094f17ba2974
Daniel <dsouzaborini@gmail.com>
2021-04-14 18:58:12 -0300

    corrige erros do lint

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function cartItemClickListener(event) {
-  console.log(event);
-}
+// function cartItemClickListener(event) {
+//   console.log(event);
+// }
 
 async function getProducts(search) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
@@ -8,28 +8,13 @@ async function getProducts(search) {
   return products.results;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-function renderList(products) {
-  const sectionList = document.getElementsByClassName('items');
-  // const computers = getProducts("computador");
-  products.forEach(({ id, title, thumbnail }) => {
-    const elementoLista = createProductItemElement({ sku: id, name: title, image: thumbnail });
-    sectionList[0].appendChild(elementoLista);
-  });
-}
-
-window.onload = async function onload() { 
-  console.log('Verificação onload');
-  const computers = await getProducts('computador');
-  renderList(computers);
-};
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -56,6 +41,21 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
+
+function renderList(products) {
+  const sectionList = document.getElementsByClassName('items');
+  // const computers = getProducts("computador");
+  products.forEach(({ id, title, thumbnail }) => {
+    const elementoLista = createProductItemElement({ sku: id, name: title, image: thumbnail });
+    sectionList[0].appendChild(elementoLista);
+  });
+}
+
+window.onload = async function onload() { 
+  console.log('Verificação onload');
+  const computers = await getProducts('computador');
+  renderList(computers);
+};
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
