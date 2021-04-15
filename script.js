eab98106b23a7b331b1582d2471685efdae3d6e0
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-04-15 18:39:30 -0300

    18:39

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
-const ol = document.querySelector('.cart__item');
+
 const fetchProducts = ((product) => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}"`)
   .then((response) => response.json())
