2c616d134700f52bdff6a056c062dfd0010f3706
Matheus H. Gois <matheus.hengo@gmail.com>
2021-04-14 21:14:40 -0300

    Cart Items added/removed to localStorage

--- script.js
+++ script.js
@@ -29,11 +29,26 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+let savedCartItems = [];
+
+const cartItemClickListener = (event) => {
+  let cartItemsList = event.target.parentNode.children;
+  cartItemsList = [...cartItemsList];
+  const cartItem = cartItemsList.reduce((acc, element, index) => {
+    let current = acc;
+    if (element === event.target) current = index;
+    return current;
+  }, 0);
+  savedCartItems.splice(cartItem, 1);
+  localStorage.setItem('cartItems', JSON.stringify(savedCartItems));
+  event.target.remove();
+};
+
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', (event) => { event.target.remove(); });
+  li.addEventListener('click', (event) => { cartItemClickListener(event); });
   return li;
 };
 
@@ -43,6 +58,17 @@ const renderCardItem = (item) => {
   cartItems.appendChild(cardItem);
 };
 
+const restoreSavedCartItems = () => {
+  if (localStorage.getItem('cartItems')) {
+    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
+    savedCartItems = [];
+    cartItems.forEach((cartItem) => {
+      savedCartItems.push(cartItem);
+      renderCardItem(cartItem);
+    });
+  }
+};
+
 // Retorna o ID do elemento
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
@@ -50,6 +76,9 @@ const itemClickListener = async (event) => {
   // Retorna o pai do elemento clicado.
   const item = event.target.parentNode;
   const itemObj = await getItemById(getSkuFromProductItem(item));
+  savedCartItems.push(itemObj);
+  // Salva no localStorage o objeto como string.
+  localStorage.setItem('cartItems', JSON.stringify(savedCartItems));
   renderCardItem(itemObj);
 };
 
@@ -78,4 +107,5 @@ const renderItems = async () => {
 
 window.onload = async () => {
   await renderItems();
+  restoreSavedCartItems();
 };
