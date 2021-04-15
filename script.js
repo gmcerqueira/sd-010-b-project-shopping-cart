3dc9415886673814f2f458d141ed83e25a5bfcc3
Daniel <dsouzaborini@gmail.com>
2021-04-15 01:29:48 -0300

    add funçao para add itens no carrinho

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// function cartItemClickListener(event) {
-//   console.log(event);
-// }
+ function cartItemClickListener(event) {
+   console.log("cliclou")
+}
+
+// const buttons = document.getElementsByClassName('item__add');
+// buttons.addEventListener('click', addToCart);
 
 async function getProducts(search) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
@@ -8,13 +11,22 @@ async function getProducts(search) {
   return products.results;
 }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+async function addToCart(id) {
+  const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const product = await response.json();
+  const {title, price} = product;
+  const cartItem = createCartItemElement({ sku: id, name: title, salePrice: price });
+  const sectionListCart = document.getElementsByClassName("cart__items");
+  sectionListCart[0].appendChild(cartItem);
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -55,8 +67,13 @@ window.onload = async function onload() {
   console.log('Verificação onload');
   const computers = await getProducts('computador');
   renderList(computers);
+  buttons[0].addEventListener("click", function(event) {
+  dadId = event.target.parentNode.firstChild.innerText;
+  console.log(dadId);
+})
 };
 
+const buttons = document.getElementsByClassName('item__add');
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
