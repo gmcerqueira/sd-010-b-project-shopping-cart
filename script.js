3f17a3390a1888ebac76a5fac15d59ce7a6505ed
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-04-15 16:36:57 -0300

    requisito 2 - adicione o produto ao carrinho de compras

--- script.js
+++ script.js
@@ -31,22 +31,35 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // comentário para primeiro commit
-// }
+function cartItemClickListener(_event) {}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return document.querySelector('.cart__items').appendChild(li);
+}
+
+const addToCart = (event) => {
+  const id = event.currentTarget.querySelector('.item__sku').innerText;
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => response.json())
+    .then((data) =>
+      createCartItemElement({
+        sku: data.id,
+        name: data.title,
+        salePrice: data.price,
+      }));
+};
 
 window.onload = function onload() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json())
     .then((data) => {
       data.results.forEach((element) => createProductItemElement(element));
+
+      const allItems = document.querySelectorAll('.item');
+      allItems.forEach((item) => item.addEventListener('click', addToCart));
     });
 };
