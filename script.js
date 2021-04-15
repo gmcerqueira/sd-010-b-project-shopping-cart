0dd1a5935ba9dae7e64a0db16b21b10d0218c7d6
walterlbf <walter.lbf.f@gmail.com>
2021-04-15 16:33:56 -0400

    requisito 1, lista de produtos criada

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-window.onload = function onload() { };
+const apiAcess = async () => {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
+  const {
+    results,
+  } = await response.json();
+  return results;
+};
 
+const newChild = (element) => {
+  const createChild = document.getElementsByClassName('items')[0];
+  createChild.appendChild(element);
+};
+
+const makeProduct = (apiJason) => {
+  const results = apiJason.forEach((element) => {
+    const object = {
+      sku: element.id,
+      name: element.title,
+      image: element.thumbnail,
+    };
+    newChild(createProductItemElement(object));
+  });
+  return results;
+};
+
+// cria elemento img no html
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
-
+// cria elemento com o texto com a classe
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
-
-function createProductItemElement({ sku, name, image }) {
+// 
+function createProductItemElement({
+  sku,
+  name,
+  image,
+}) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -34,10 +62,21 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({
+  sku,
+  name,
+  salePrice
+}) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+const createList = async () => {
+  makeProduct(await apiAcess());
+};
+
+window.onload = function onload() {
+  createList();
+};
