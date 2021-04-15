8b2adc55dc0247e45c272555483bfe500d2768ee
Arlen Freitas <arlenfreitas1@gmail.com>
2021-04-15 17:45:19 -0300

    save local and load

--- script.js
+++ script.js
@@ -12,100 +12,74 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function saveItemsCart() {
+  const cartItems = document.getElementsByClassName('cart__items')[0];
+  localStorage.setItem('cartList', JSON.stringify(cartItems.innerHTML));
+}
+
+function cartItemClickListener(event) {
+  if (event.target.parentNode) {
+    event.target.parentNode.removeChild(event.target);
+    saveItemsCart();
+  }
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const cartItems = document.getElementsByClassName('cart__items')[0];
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  cartItems.appendChild(li);
+  return li;
+}
+
+function moveItemsToCart(button) {
+  button.addEventListener('click', (event) => {
+    const sku = getSkuFromProductItem(event.target.parentNode);
+    fetch(`https://api.mercadolibre.com/items/${sku}`)
+      .then((data) => data.json())
+      .then((data) => { 
+        createCartItemElement(data);
+        saveItemsCart();
+      });
+  });
+}
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const primarySection = document.getElementsByClassName('items')[0];
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  moveItemsToCart(button);
+  section.appendChild(button);
+  primarySection.appendChild(section);
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+const createItemsList = async () => {
+const result = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+const loading = document.getElementsByClassName('loading')[0];
+loading.parentNode.removeChild(loading);
+const data = await result.json();
+data.results.forEach((product) => { createProductItemElement(product); });
+};
+
+function loadCartItems() {
+  const cartItems = document.getElementsByClassName('cart__items')[0];
+  cartItems.innerHTML = JSON.parse(localStorage.getItem('cartList'));
+  cartItems.addEventListener('click', cartItemClickListener);
 }
 
-function cartItemClickListener(event) {
-  const parentNode = event.target.parentElement;
-  parentNode.removeChild(event.target);
-}
-
-// carItemClickListener cria um evento ao clicar em um item da lista, removendo
-// mesma logica do moveItensToCart
-// no elemento pai clico na const parentNode que eh o event target elemento pai
-// escolho a child que vou remover da lista de itens que cliquei event.target
-// indo pro parentNode que remove o filho, o item que cliquei 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-function createItemsList() {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector('.loading').remove();
-      data.results.forEach((item) => {
-        const object = {
-          sku: item.id,
-          name: item.title,
-          image: item.thumbnail,
-        };
-        document.querySelector('.items').appendChild(createProductItemElement(object));
-      });
-    });
-}
-// createItemsList function que faz um fetch da api
-// trazendo os itens e transformados no .then em .json()
-// apos pegar os dados do data.result
-// acesso results com forEach pois ele eh um array
-// forEach passa por todos os dados buscando sku, name e image
-// com esses dados gero uma lista com o resultado desses itens usando appendChild no (createProductItemElement).
-
-// Ao esperar a requisicao da API, adiciono 'loading'
-// crio uma tag h2 com a class 'loading' que vai ser carregada e removida
-// usando querySelector e .remove
-// quando terminar de carregar api.
-
-function moveItensToCart() {
-  document.querySelector('.items').addEventListener('click', (event) => {
-    if (event.target.classList.contains('item__add')) {
-      const parentNode = event.target.parentElement;
-      const sku = getSkuFromProductItem(parentNode);
-      fetch(`https://api.mercadolibre.com/items/${sku}`)
-        .then((response) => response.json())
-        .then((data) => {
-          const idObject = {
-            sku,
-            name: data.title,
-            salePrice: data.price,
-          };
-          document.querySelector('.cart__items').appendChild(createCartItemElement(idObject));
-        });
-    }
-  });
-}
-// moveItensToCart adiciono produto no carrinho
-// uso addEventLister para adicionar ao carrinho com click
-// id eh o sku do Readme
-// ao clicar no botao, ele pega os itens que cliquei e joga no ol com querySelector('items)
-// usando eventListener que ao clicar ele me da o event target com tudo
-// crio a const sku, uso a funcao ja existente (getSkuFromProductItem) que busca o ID
-// passo o elemento parentNode que eh o item clicado
-// ele busca no elemento pai, se existe span, class, item__sku, id
-// faco fetch buscado especificamente conforme readme link
-// e transformo em json()
-// atraves do data pego os dados recebidos e crio uma const idObject
-// que contem o id, name e salePrice 
-// uso querySelector do cart__items e faco appendChield usando a funcao ja criada
-// (createCartItemElement) passando como parementro o meu idObject 
-window.onload = function onload() {
+window.onload = function onload() { 
   createItemsList();
-  moveItensToCart();
- };
+  loadCartItems();
+};
