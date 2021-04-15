c715d125349008d01b2b2d59e32f2af3ea0c5064
Camila Arruda <camilaam1802@gmail.com>
2021-04-15 17:01:43 -0300

    resolução do requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,6 +12,8 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Primeiro Passo
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -26,20 +26,49 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
 }
-
-function createCartItemElement({ sku, name, salePrice }) {
+ */
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
 
 // Primeira modificação
+
+async function getElements(QUERY) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
+  const { results } = await response.json();  
+  // console.log(results);
+  return results; 
+}
+
+const appendProducts = (element) => document.getElementsByClassName('items')[0]
+.appendChild(element); 
+
+const renderProduct = (prodctJson) => {
+  prodctJson.forEach((product) => {
+    const objResult = {
+      sku: product.id,
+      name: product.title,
+      image: product.thumbnail,
+    };
+    appendProducts(createProductItemElement(objResult));
+  });
+};
+
+const allProductsChamada = async () => {
+ renderProduct(await getElements('computador')); 
+};
+
+window.onload = function onload() { 
+  allProductsChamada();
+};
