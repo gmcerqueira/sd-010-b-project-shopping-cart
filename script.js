4c1c6864e9bd8880b00e8062f51fee1d6c140e80
Joao Vanelli <vanellijoao@gmail.com>
2021-04-15 15:01:12 -0300

    Req 2 - Adiciona produtos ao carrinho de compras

--- script.js
+++ script.js
@@ -14,21 +14,10 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-// create the product in a 'section' element with the className 'item'. that 'section' has the 'sku', 'name', 'img' (all 3 from the API) and a button.
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  return section;
+// get the sku from the item clicked
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
-
-// NO IDEA YET --- get the sku from the item, but no idea why.....
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
 
 // delete the product from the shopping cart when clicked
 // function cartItemClickListener(event) {
@@ -36,42 +25,66 @@ function createProductItemElement({ sku, name, image }) {
 // }
 
 // create the 'li' element that will be inside the shopping cart sidebar
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// after clicking on a add button, adds that item to the shopping cart
+async function addToTheCart() {
+  const productId = getSkuFromProductItem(this.parentElement);
+  const response = await fetch(`https://api.mercadolibre.com/items/${productId}`);
+  const product = await response.json();
+  
+  const { id: sku, title: name, price: salePrice } = product;
+
+  const shoppingCart = document.querySelector('ol.cart__items');
+  shoppingCart.appendChild(createCartItemElement({ sku, name, salePrice }));
+  console.log(product);
+} 
 
 // get the array results from the API
 async function getData() {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const json = await response.json();
-  const products = await json.results;
+  const products = json.results;
   return products;
 }
 
 // create each product object containing sku, name, img and salePrice
+// uses getData()
 async function createProductsObject() {
-    const products = await getData();
-    return products.reduce((accumulator, product) => 
+  const products = await getData();
+  return products.reduce((accumulator, product) => 
     [...accumulator,
       {
         sku: product.id,
         name: product.title,
         image: product.thumbnail,
-        salePrice: product.price,
       },
     ], []);
 }
 
+// create the product in a 'section' element with the className 'item'. that 'section' has the 'sku', 'name', 'img' (all 3 from the API) and a button.
+// also add an eventListener on the created button, to 
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', addToTheCart);
+  return section;
+}
 // create the card of each product
+// uses createProductsObject() and createProductItemElement()
 async function renderProduct() {
   const products = await createProductsObject();
   const itemsSection = document.querySelector('section.items');
-  console.log(products);
-  console.log(itemsSection);
   products.forEach(({ sku, name, image }) => {
     itemsSection.appendChild(createProductItemElement({ sku, name, image }));
   });
