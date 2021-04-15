c1a8b983619f1f3c59f790adb1589d7b20b9470a
denis-rossati <denis.rossati@hotmail.com>
2021-04-15 09:34:30 -0300

    uma noite de sono fez muito bem, requisito 2, 3 e 7 feitos

--- script.js
+++ script.js
@@ -24,22 +24,25 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-/* function getSkuFromProductItem(item) {
+function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-} */
+}
 
-/* function cartItemClickListener(event) {
+function cartItemClickListener(event) {
   // coloque seu código aqui
-} */
+  event.target.remove();
+}
 
-/* function createCartItemElement({ sku, name, salePrice }) {
+async function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
 
+// requisito 1 ------------------------------------------------------------------------------------
+// essa função acessa os array "results" da API
 async function getResultsAPI(url) {
   const { results } = await fetch(url)
   .then((response) => response.json())
@@ -47,16 +50,54 @@ async function getResultsAPI(url) {
   return results;
 }
 
+// Essa função chama executa uma chamada para a função que monta o elemento;
 async function createItemElements(url) {
   const sectionItems = document.querySelector('.items');
+  
+  const APILoading = '<div class = "loading" >loading...</div>';
+  sectionItems.innerHTML = APILoading;
+  const getLoading = document.querySelector('.loading');
+  
   const createHTMLElements = await getResultsAPI(url)
   .then((array) => {
+    getLoading.remove();
     array.forEach((element) => sectionItems.appendChild(createProductItemElement(element)));
   })
-  .catch(() => 'não foi possível acessar o elemento');
+  .catch(() => getLoading.remove());
   return createHTMLElements;
 }
 
-window.onload = function onload() {
-  createItemElements('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+// requisito 2 ------------------------------------------------------------------------------------
+
+function executeFunctionWhenClick() {
+  const getOlList = document.querySelector('.cart__items');
+  const ItemsGrid = document.querySelectorAll('.item');
+  ItemsGrid.forEach(async (item) => {
+    item.lastChild.addEventListener('click', async () => {
+      const idItem = getSkuFromProductItem(item);
+
+      const itemInfo = await fetch(`https://api.mercadolibre.com/items/${idItem}`)
+      .then((response) => response.json())
+      .then((data) => data);
+
+      getOlList.appendChild(await createCartItemElement(itemInfo));
+    });
+  });
+}
+
+// requisito 3 ------------------------------------------------------------------------------------
+// código na linha 31
+
+// requisito 4 ------------------------------------------------------------------------------------
+
+// requisito 5 ------------------------------------------------------------------------------------
+
+// requisito 6 ------------------------------------------------------------------------------------
+
+// requisito 7 ------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+window.onload = async function onload() {
+  await createItemElements('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  await executeFunctionWhenClick();
  };
