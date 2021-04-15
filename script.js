c10393fdb06ba4512370a0fb4edb428da032d5ef
Hugo Simoes <hugolfsimoes@gmail.com>
2021-04-15 12:53:35 -0300

    dicionado comentarios explicando cada funçao

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// cria uma tag de imgem e atribui informções a partir dos parâmetros.
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -9,12 +10,18 @@ function createProductImageElement(imageSource) {
   return item.querySelector('span.item__sku').innerText;
 }
 */
+
+// Quando um item da lista do carrinho de compras é clicado o mesmo é removido da lista.
 function cartItemClickListener(event) {
   const alvo = event.target;
   const listCart = document.querySelector('.cart__items');
   listCart.removeChild(alvo);
+
+  const local = localStorage.getItem('product', alvo.innerHTML);
+  console.log(local);
 }
 
+// Recebe um objeto desconstrói pegando apenas id,title e price, cria um elemento li com informações do objeto recebido, adiciona um escutador de eventos com a função 'cartItemClickListener' e retorna o elemento li.
 function createCartItemElement({
   id: sku,
   title: name,
@@ -27,6 +34,7 @@ function createCartItemElement({
   return li;
 }
 
+// Recebe as section criadas e adiciona um escutador de eventos click, faz um requerimento em uma API passando o id pego na section, traansforma em JSON, chama a função 'createCartItemElement' e adiciona o retono na lista de compras.
 function addToCart(product) {
   product.addEventListener('click', async () => {
     const id = product.children[0].innerText;
@@ -37,6 +45,7 @@ function addToCart(product) {
   });
 }
 
+// Cria e adiciona informações ao elemento de acordo com parâmetros passados.
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -45,6 +54,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Recebe um objeto como parâmetro, cria uma section e coloca dentro da section os elementos criados com informações do objeto, chama a função addToCart passando  section crida e retorna o elemento.
 function createProductItemElement({
   id: sku,
   title: name,
@@ -62,6 +72,7 @@ function createProductItemElement({
   return section;
 }
 
+// Faz  requisição da API transforamando em JSON, e envia cada item para ser criado na função 'createProductItemElement' e adiciona cada item na section. 
 async function getProductsList(word) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${word}`);
   const object = await response.json();
@@ -77,4 +88,6 @@ async function getProductsList(word) {
 
 window.onload = function onload() {
   getProductsList('computador');
+
+  /*  const listStorage = localStorage.getItem('product'); */
 };
