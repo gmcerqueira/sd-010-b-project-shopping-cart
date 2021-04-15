a4ee7e6323bb14bf0fc1e96d511860f48c8013c2
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-04-15 17:56:42 -0300

    Requesitos 3 e 4

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+const array = [];
+function retirarItem(valor) {
+  console.log(valor);
+    const element = array.find((el) => el === valor);
+    array.splice(array.indexOf(element), 1);
+    localStorage.setItem('IDS', JSON.stringify(array));
+}
+
 function cartItemClickListener(event) {
-  event.target.remove();
+  if (event.target.parentElement.parentElement.tagName === 'LI') {
+    retirarItem(event.target.parentElement.firstChild.innerText.split(' ')[1]);
+    event.target.parentElement.parentElement.remove();
+  } else {
+    retirarItem(event.target.firstChild.firstChild.innerText.split(' ')[1]);
+    event.target.remove();
+  }
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -27,6 +41,8 @@ function createCustomElement(element, className, innerText) {
 function buttonAdd(button, sku) {
   button.addEventListener('click', () => {
     const $ItemID = sku;
+    array.push(sku);
+    localStorage.setItem('IDS', JSON.stringify(array));
     fetch(`https://api.mercadolibre.com/items/${$ItemID}`)
     .then((r) => r.json())
     .then((r) => {
@@ -71,8 +87,25 @@ function verifiedFetch(url) {
     });
 }
 
+function sacolaSalva(element) {
+    const $ItemID = element;
+    fetch(`https://api.mercadolibre.com/items/${$ItemID}`)
+    .then((g) => g.json())
+    .then((g) => {
+      const object = {
+        sku: $ItemID, 
+        name: g.title, 
+        salePrice: g.price,
+      };
+      const cart = document.getElementsByClassName('cart__items')[0];
+      cart.appendChild(createCartItemElement(object));
+    });
+}
+
 window.onload = function onload() { 
   verifiedFetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
+  const ar = JSON.parse(localStorage.getItem('IDS'));
+  ar.forEach((element) => sacolaSalva(element));
 };
 
 // function getSkuFromProductItem(item) {
