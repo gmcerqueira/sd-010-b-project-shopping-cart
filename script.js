79f2ff8b994c1e4cca4098b6b5f71a7cb0435ae4
EderPaiva <edergomesdepaiva12@gmail.com>
2021-04-15 08:55:52 -0300

    nomes undefined entender o motivo

--- script.js
+++ script.js
@@ -3,21 +3,6 @@ window.onload = async function onload() {
 
 };
 
-async function getComputer() {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computer');
-  const computer = await response.json();
-  const { results } = computer;
-  // ate aqui está tudo certo estou recebendo um objeto 
-  computer.forEach ((computer) => {
-    const newComputerObject = {
-      sku: computer.id, 
-      name: computer.name,
-      image: computer.thumbnail,
-    };
-    document.querySelector('.items').appendChild(createProductItemElement(computer));
-  })
-}
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -44,18 +29,34 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+async function getComputer() {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computer');
+  const computers = await response.json();
+  const { results } = computers;
+  // ate aqui está tudo certo estou recebendo um objeto 
+  results.forEach((computer) => {    
+    const newComputerObject = {
+      sku: computer.id, 
+      name: computer.name,
+      image: computer.thumbnail,
+    };
+    document.querySelector('.items').appendChild(createProductItemElement(newComputerObject));
+  });
 }
+getComputer();
