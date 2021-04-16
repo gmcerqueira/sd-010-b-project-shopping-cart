9ba3be4bfe5c6d3b3ef1fed07d4f3416087d3370
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-04-16 09:24:54 -0300

    add exercicio 7

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let loadingStatus = false;
+function verifyRequestAPI() {
+  if (loadingStatus) {
+    const spanLoading = document.createElement('span');
+    spanLoading.className = 'loading';
+    spanLoading.innerText = 'loading...';
+    document.body.appendChild(spanLoading);
+  } else if (loadingStatus === false) {
+    document.querySelector('.loading').remove();
+  }
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -22,11 +34,11 @@ function cartItemClickListener(event) {
 
   const liText = event.target.innerText;
   const price = Number(liText.substring(liText.indexOf('$') + 1, liText.length));
-
+  
   sum -= price;
   arr = [Math.abs(sum)];
 
-  spanSum.innerText = `${Math.abs(sum.toFixed(3))}`;
+  spanSum.innerText = `${Math.abs(sum.toFixed(2))}`;
   
   localStorage.setItem('listCart', document.getElementsByClassName('cart__items')[0].innerHTML);
   localStorage.setItem('sum', Math.abs(sum));
@@ -45,7 +57,7 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   arr.push(Number(liText.substring(liText.indexOf('$') + 1, liText.length)));
   sum = arr.reduce((acumulator, currentValue) => acumulator + currentValue);
   
-  spanSum.innerHTML = `${Math.abs(sum.toFixed(3))}`;
+  spanSum.innerHTML = `${Math.abs(sum.toFixed(2))}`;
 
   cartItems.appendChild(li);
   localStorage.setItem('listCart', cartItems.innerHTML);
@@ -53,9 +65,16 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 }
 
 function fetchID(id) {
+  loadingStatus = true;
+  verifyRequestAPI();
+
   fetch(`https://api.mercadolibre.com/items/${id}`)
   .then((data) => data.json())
-  .then((product) => createCartItemElement(product));
+  .then((product) => {
+    loadingStatus = false;
+    verifyRequestAPI();
+    createCartItemElement(product);
+ });
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
@@ -78,6 +97,8 @@ function fetchProduct(computer) {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${computer}`)
     .then((response) => response.json())
     .then(({ results }) => {
+      loadingStatus = false;
+      verifyRequestAPI();
       results.forEach((data) => createProductItemElement(data));
     });
 }
@@ -91,25 +112,32 @@ function removeAllItems() {
   const btnRemove = document.querySelector('.empty-cart');
   btnRemove.addEventListener('click', () => {
     cartItems.innerHTML = '';
+    const spanSum = document.getElementsByClassName('total-price')[0];
+    spanSum.innerHTML = 0;
+    arr = [0];
+    localStorage.setItem('listCart', cartItems.innerHTML);
+    localStorage.setItem('sum', 0);
   });
 }
 
 function loadStorage() {
-  const spanSum = document.querySelector('.total-price');
-  const sumLocalStorage = Number(localStorage.getItem('sum'));
-  spanSum.innerHTML = sumLocalStorage;
   const items = document.getElementsByTagName('ol')[0];
+  const spanSum = document.querySelector('.total-price');
+  
+  const sumLocalStorage = Number(localStorage.getItem('sum'));
+  spanSum.innerHTML = Math.abs(sumLocalStorage.toFixed(2));
   
   items.addEventListener('click', (event) => {
     event.target.remove();
-    const liText = event.target.innerText;
-    const price = Number(liText.substring(liText.indexOf('$') + 1, liText.length));
-    console.log(price);
     
-    localStorage.setItem('listCart', items.innerHTML);
-    localStorage.setItem('sum', sum);
-  });
+    const liText = event.target.innerText;
+    const price2 = Number(liText.substring(liText.indexOf('$') + 1, liText.length));
+    console.log(price2);
 
+    localStorage.setItem('listCart', items.innerHTML);
+  });
+  
+  localStorage.setItem('sum', sum);
   items.innerHTML = localStorage.getItem('listCart');
 }
 
@@ -117,4 +145,6 @@ window.onload = function onload() {
   fetchProduct('computador');
   removeAllItems();
   loadStorage();
+  loadingStatus = true;
+  verifyRequestAPI();
 };
