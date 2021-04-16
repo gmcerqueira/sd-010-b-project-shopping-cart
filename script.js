d658b4b713e8314b66e2bd126bb9d893d0ec8caf
root <diegoserafim1@gmail.com>
2021-04-16 18:11:22 -0300

    criada a funçao getComputer para pegar products da api e inserir no selector .items

--- script.js
+++ script.js
@@ -5,38 +5,54 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-// ASYNC declara a função como assincrona! e assim podemos usar o AWAIT
-async function returnComputer() {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
-  const computer = await response.json();
-  // console.log(computer);
-  return computer;
+// function renderComputer({ url }, index) {
+//   const elementImgComputer = createProductImageElement(url);
+//   console.log(elementImgComputer);
+// }
+
+// function renderComputers(computers) {
+//   const elementComputers = document.getElementsByClassName('items');
+//   computers.forEach(({url}) => {
+//     const elementImgComputers = createProductImageElement(url);
+//     elementComputers.appendChild(elementImgComputers);
+//   });
+// }
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
 }
 
-window.onload = function onload() {
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
+}
+
+// ASYNC declara a função como assincrona! e assim podemos usar o AWAIT
+async function getComputer() {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const { results } = await response.json();
+  results.forEach((product) => {
+    const getComputers = createProductItemElement(product);
+    document.querySelector('.items').appendChild(getComputers);
+    console.log(getComputers);
+    return getComputers;
+  });
+}
+
+window.onload = function () {
   console.log('Funcionando, corretamente.');
-  returnComputer();
-  createProductImageElement();
+  getComputer();
 };
-
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
-
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
-
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-//   return section;
-// }
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
