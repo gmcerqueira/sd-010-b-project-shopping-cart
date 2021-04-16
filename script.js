aac25e4f0af9ca8e3b19c7e49cf24b00591e64d9
Junior030 <juniorhenrique030@gmail.com>
2021-04-16 20:42:57 -0300

    Implementando: A renderizacao dos items da API, Add um item ao carrinho e Removendo item do carrinho ao clicar nele

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -30,14 +28,42 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
+async function cartItemClickListener(event) {
   // coloque seu código aqui
+  event.path[0].remove();
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+async function getListProducts() {
+  const endPoint = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const products = await endPoint.json();
+  const span = document.querySelector('.items');
+  products.results.forEach((product) => {
+    span.appendChild(createProductItemElement(product));
+  });
+  return span;
+}
+
+function shoppingCart(spans) {
+  const ol = document.querySelector('.cart__items');
+  for (let index = 0; index < spans.children.length; index += 1) {
+    spans.children[index].lastChild.addEventListener('click', async (button) => {
+      const id = button.path[1].firstChild.innerText;
+      const item = await fetch(`https://api.mercadolibre.com/items/${id}`);
+      const itemJson = await item.json();
+      ol.appendChild(createCartItemElement(itemJson));
+    });
+  }
+}
+
+window.onload = async function onload() {
+  const products = await getListProducts();
+  shoppingCart(products);
+};
