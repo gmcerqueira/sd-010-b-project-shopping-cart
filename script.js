e4aaf642904201338b02076effe309c546c36eef
Thiago Kachinsky <tjk091@gmail.com>
2021-04-16 16:56:27 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 /* eslint-disable max-lines-per-function */
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,9 +13,12 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+let getPcInfos = [];
+
 // Remover os itens com o clique
 function cartItemClickListener(event) {
   event.target.remove();
+  localStorage.removeItem('pcs');
 }
 
 // Estrututa dos itens na tela
@@ -30,15 +31,17 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 // Estrutura dos itens no carrinho de compras
-const addProductToCart = (itemId) => {
-  fetch(`https://api.mercadolibre.com/items/${itemId}`)
-  .then((response) => response.json())
-  .then((result) => {
-    const product = { sku: result.id, name: result.title, salePrice: result.price };
+const addProductToCart = async (itemId) => {
+  const getCartItems = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+  const cartItem = await getCartItems.json();
+  const { id, title, price } = cartItem;
+  const product = { sku: id, name: title, salePrice: price };
 
-    const selectItem = document.querySelector('.cart__items');
-    selectItem.appendChild(createCartItemElement(product));
-  });
+  const selectItem = document.querySelector('.cart__items');
+  selectItem.appendChild(createCartItemElement(product));
+
+  getPcInfos.push(product);
+  localStorage.setItem('pcs', JSON.stringify(getPcInfos));
 };
 
 // Captura o id de cada item
@@ -62,19 +65,39 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+// Busca os produtos desejados da API do MercadoLivre
 const getProduct = async () => {
   const product = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const result = await product.json();
   const { results } = result;
-    const resultsApi = results.map(({ id, title, thumbnail }) => 
+  const resultsApi = results.map(({ id, title, thumbnail }) => 
       createProductItemElement({ sku: id, name: title, image: thumbnail }));
-
+      
     const selectItem = document.querySelector('.items');
     
     resultsApi.forEach((element) => {
       selectItem.appendChild(element);
     });
-    console.log(result);
 };
 
-window.onload = () => getProduct();
+// Armazena itens no localStorage
+function localStorageCache() {
+  const getPcInfosLocalStorage = localStorage.getItem('pcs');
+  getPcInfos = getPcInfosLocalStorage ? JSON.parse(getPcInfosLocalStorage) : [];
+  const selectItem = document.querySelector('.cart__items');
+
+  getPcInfos.map(({ sku, name, salePrice }) => {
+    const li = document.createElement('li');
+    li.className = 'cart__item';
+    li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+    li.addEventListener('click', cartItemClickListener);
+    selectItem.appendChild(li);
+    return li;
+  });
+}
+
+window.onload = () => {
+  localStorageCache();
+
+  getProduct();
+};
