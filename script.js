4fe13f758b641ba379d2a17124122005b4f9b78d
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-04-16 20:31:39 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -29,11 +29,15 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }, ca
   return section;
 }
 
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   const containerItems = document.querySelector('.cart__items');
   containerItems.appendChild(li);
   return li;
@@ -58,10 +62,6 @@ async function requisitionProduct(product) {
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
 // }
 
 window.onload = function onload() {
