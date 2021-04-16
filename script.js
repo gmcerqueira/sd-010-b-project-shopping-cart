235261ba9c58dcd7f3fdeba98b441d8179473c90
Daniel <dsouzaborini@gmail.com>
2021-04-16 01:32:28 -0300

    corrige requisito 4

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-  function cartItemClickListener(event) {
-    const itemToDelete = event.target;
-    // Método por Krasimir, stackoverflow.
-    // Disponível em: https://stackoverflow.com/questions/18795028/javascript-remove-li-without-removing-ul
-    console.log(itemToDelete.parentNode.children);
-    let position;
-    const listOfChildren = itemToDelete.parentNode.children;
-    for (let index = 0; index < listOfChildren.length; index += 1) {
-      if (listOfChildren[index] === itemToDelete) position = index;
-    }
-    itemToDelete.parentNode.removeChild(itemToDelete);
-    const savedIds = localStorage.getItem('cart');
-    console.log(position);
-    const arrayIds = savedIds.split(',');
-    const removed = arrayIds.filter((item) => (arrayIds.indexOf(item)) !== position);
-    localStorage.setItem('cart', (removed));
+function cartItemClickListener(event) {
+  const itemToDelete = event.target;
+  // Método por Krasimir, stackoverflow.
+  // Disponível em: https://stackoverflow.com/questions/18795028/javascript-remove-li-without-removing-ul
+  console.log(itemToDelete.parentNode.children);
+  let position;
+  const listOfChildren = itemToDelete.parentNode.children;
+  for (let index = 0; index < listOfChildren.length; index += 1) {
+    if (listOfChildren[index] === itemToDelete) position = index;
   }
+  itemToDelete.parentNode.removeChild(itemToDelete);
+  const savedIds = localStorage.getItem('cart');
+  console.log(position);
+  const arrayIds = savedIds.split(',');
+  const removed = arrayIds.filter((item) => (arrayIds.indexOf(item)) !== position);
+  localStorage.setItem('cart', (removed));
+}
 
 async function getProducts(search) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
@@ -22,12 +22,11 @@ async function getProducts(search) {
   return products.results;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, price }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${price}`;
   li.addEventListener('click', cartItemClickListener);
-
   return li;
 }
 
@@ -37,7 +36,7 @@ async function addToCart(id) {
   const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
   const product = await response.json();
   const { title, price } = product;
-  const cartItem = createCartItemElement({ sku: id, name: title, salePrice: price });
+  const cartItem = createCartItemElement({ sku: id, name: title, price });
   const sectionListCart = document.getElementsByClassName('cart__items');
   sectionListCart[0].appendChild(cartItem);
   cartIds.push(id);
@@ -61,12 +60,10 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
@@ -78,25 +75,35 @@ function renderList(products) {
   });
 }
 
+// Função ForEach assícrono, por Sebastien Chopin.
+// Disponível em: https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+// async function asyncForEach(array, callback) {
+//   for (let index = 0; index < array.length; index += 1) {
+//     await callback(array[index], index, array);
+//   }
+// }
+
 async function restoreSavedCart() {
   const savedIds = localStorage.getItem('cart');
   savedIds.split(',').forEach((id) => addToCart(id));
+  // asyncForEach(savedIds.split(','), (id) => addToCart(id));
 }
 
 const buttons = document.getElementsByClassName('item__add');
 
-window.onload = async function onload() { 
+window.onload = async function onload() {
   console.log('Verificação onload');
   const computers = await getProducts('computador');
   renderList(computers);
   if (localStorage.getItem('cart') !== null && localStorage.getItem('cart') !== '') {
     restoreSavedCart();
   }
-  Object.values(buttons).forEach((button) => {
-    button.addEventListener('click', function (event) {
-      const dadId = event.target.parentNode.firstChild.innerText;
-      addToCart(dadId);
-      console.log(dadId);
-    });
+
+Object.values(buttons).forEach((button) => {
+  button.addEventListener('click', function (event) {
+    const dadId = event.target.parentNode.firstChild.innerText;
+    addToCart(dadId);
+    console.log(dadId);
   });
+});
 };
