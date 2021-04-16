e5281f4994cdfc7c642c633c105c94bd69c9a8ef
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-16 19:43:40 -0300

    Requisito 3 realizado

--- script.js
+++ script.js
@@ -28,19 +28,19 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
   
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  event.target.remove();
+}
     
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
   
-const VerifiedFetch = async () => {
+const consultAPI = async () => {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
   const responseObjeto = await response.json();
   const arrayResult = responseObjeto.results;
@@ -82,7 +82,7 @@ const eventButton = () => {
 };
 
 window.onload = async function onload() { 
-  const products = await VerifiedFetch();
+  const products = await consultAPI();
   renderProduct(products);
   eventButton();
 };
