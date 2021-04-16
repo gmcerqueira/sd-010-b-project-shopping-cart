e64b0d7ca634ed4718153604857ad9d3265591b9
renanpbraga <renanpbraga@gmail.com>
2021-04-16 19:17:54 -0300

    corrige erros de lin requisito 1

--- script.js
+++ script.js
@@ -24,13 +24,13 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
@@ -53,8 +53,7 @@ async function createListItems() {
   });
 }
  
-
-
 window.onload = function onload() {
   createListItems();
+  createCartItemElement();
 };
