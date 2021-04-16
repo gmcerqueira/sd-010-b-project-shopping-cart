2ce9f9ce54c54ec0aa6d5084d4a44d96387fd269
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-04-16 19:09:19 -0300

    testando a 2 erros slint

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 // const lista = document.getElementsByClassName('cart__items');
 // lista.appendChild(p)
 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 function criarElemento(element, className, innerText) {
   const elemento = document.createElement(element);
   elemento.className = className;
@@ -17,19 +25,29 @@ function criarImagem(imageSource) {
   return img;
 }
 
-function criarElementoNaTabela({ sku, name, image}) {
+async function clickElemente(event) {
+  const incontrarItem = document.getElementsByClassName('cart__items')[0];
+  const parent = event.target.parentElement;
+  const prodID = getSkuFromProductItem(parent);
+  const prod = await getProductId(prodID);
+  const newEl = createCartItemElement({
+    sku: prod.id,
+    name: prod.title,
+    salePrice: prod.price,
+  });
+  calcPreço(prod.price);
+  incontrarItem.appendChild(newEl);
+  localStorage.setItem('cartItems', incontrarItem.innerHTML);
+}
+
+function criarElementoNaTabela( { sku, name, image} ) {
   const encontraSection = document.createElement('section');
   encontraSection.className = 'item';
   encontraSection.appendChild(criarElemento('span', 'item__sku', sku));
   encontraSection.appendChild(criarElemento('span', 'item__title', name));
   encontraSection.appendChild(criarImagem(image));
-  const informaçoesDoproduto2 = {
-    sku: sku,
-    name: name,
-    image: image,
-  };
   const criarButton = criarElemento('button', 'item__add', 'Adicionar ao carrinho!');
-  criarButton.addEventListener('click',  clickElemente);
+  criarButton.addEventListener('click', clickElemente);
   encontraSection.appendChild(criarButton);
   return encontraSection;
 }
@@ -45,30 +63,12 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-async function clickElemente (event) {
-  const incontrarItem = document.getElementsByClassName('cart__items')[0];
-  const parent = event.target.parentElement;
-  const prodID = getSkuFromProductItem(parent);
-  const prod = await getProductId(prodID);
-  const newEl = createCartItemElement({
-    sku: prod.id,
-    name: prod.title,
-    salePrice: prod.price,
-  });
-  let total = localStorage.getItem('preco');
-  total += prod.price;
-  localStorage.setItem('preco', total);
-  console.log(total);
-  incontrarItem.appendChild(newEl);
-  localStorage.setItem('cartItems', incontrarItem.innerHTML);
-}
-
-function calcPreço(price){
-  let total =  localStorage.getItem("preco");
-  total += preco;
-  localStorage.setItem("preco", total);
-  console.log(total);
-}
+// function calcPreço(){
+//   let total =  localStorage.getItem("preco");
+//   total += preco;
+//   localStorage.setItem("preco", total);
+//   console.log(total);
+// }
 
 async function transformarUrl(url) {
   const pegarIformacoesDaUrl = await fetch(url);
@@ -92,15 +92,7 @@ async function transformarUrl(url) {
    console.log('entrou na lista');
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 window.onload = function onload() {
   transformarUrl('https://api.mercadolibre.com/sites/MLB/search?q=computador');
-  localStorage.setItem('preco', 0);
+  localStorage.setItem("preco", 0);
  };
