58a9da99ccb7c6d9a660770ce327ef16d8dfb8fc
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-04-16 22:54:57 -0300

    request 2

--- script.js
+++ script.js
@@ -19,6 +19,39 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+async function getId(sku) {
+  return fetch(`https://api.mercadolibre.com/items/${sku}`)
+  .then((response) => response.json())
+  .then((response) => (response))
+  .catch((error) => error);
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function addToCart(event) {
+  const parent = event.target.parentElement;
+  const id = getSkuFromProductItem(parent);
+  const item = await getId(id);
+  const add = createCartItemElement({
+    sku: item.id,
+    name: item.title,
+    salePrice: item.price,
+  });
+  document.querySelector('.cart__items').appendChild(add);
+}
+
+// Para resolver o requisito 2 obtive ajuda do meu colega de turma Leandro Reis, consultando seu PR para entender a lógica por trás desse requisito. src: https://github.com/tryber/sd-010-b-project-shopping-cart/pull/6
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -26,36 +59,29 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const addItem = (createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  addItem.addEventListener('click', addToCart);
+  section.appendChild(addItem);
 
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
 // function cartItemClickListener(event) {
 //   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
 // }
 
 const showItems = async () => {
   const itemData = await fetchItems();
   const itemList = document.querySelector('.items');
   itemData.forEach((item) => {
-    itemList.appendChild(createProductItemElement(item));
+    itemList.appendChild(createProductItemElement({
+      sku: item.id,
+      name: item.title,
+      image: item.thumbnail,
+    }));
   });
 };
 
 window.onload = function onload() {
-  fetchItems();
   showItems();
 };
