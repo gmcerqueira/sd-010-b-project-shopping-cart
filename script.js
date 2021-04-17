96daafd29775ee05c816cee68f41686f80ad2b09
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-04-17 01:12:57 -0300

    Em script.js: documenta as functions "createProductImageElement" e "createCustomElement"; em function "createProductItemElement", altera as chaves do objeto desestruturado recebido como parâmetro e documenta esta function; implementa a function "createProductsList" que retorna uma lista dos itens relacionados com uma consulta que contém o termo (palavra) passado como parâmetro; implementa a function "renderProductsList" que recebe uma lista de produtos e renderizar seus elementos; em "window.onload", altera function "onload" para async e executa uma chamada de "renderProductsList" com a lista retornada por "createProductsList".

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-window.onload = function onload() { };
+/*
+  Essa função recebe uma imageSource, constrói e retorna este respectivo elemento HTML com a classe "item__image".
+*/
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+/*
+  Essa função recebe um element, className e innerText, constrói e retorna este respectivo elemento HTML com estas informações.
+*/
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
-
-function createProductItemElement({ sku, name, image }) {
+/*
+  Essa função desestrutura um objeto produto recebido, construindo e retornando os componentes HTML referentes a este parâmetro.
+*/
+// function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -25,6 +33,43 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
+
+/*
+  Essa função retorna uma lista dos itens relacionados com uma consulta que contém o termo (palavra) passado como parâmetro.
+
+  Créditos pela explicação sobre async/await e .then():
+  @Henrique Clementino - Turma 10 - Tribo B 
+  @Janaina Oliveira - Turma 10 - Tribo B 
+  @Luanderson Santos 
+*/
+const createProductsList = async (query = 'computador') => {
+  // busca os resultados de uma Promise de consulta usando a api do mercado livre e um termo recebido como parâmetro
+ const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+ 
+ // desestrutura o objeto JSON resultado do sucesso de uma Promise do processamento da stream response
+ const { results } = await response.json();
+
+ return results; // retorna uma lista de produtos
+//  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+//  const { results } = await response.json();
+//  console.log(results);
+};
+
+/*
+  Essa função recebe uma lista de produtos e renderizar seus elementos.
+
+  Créditos pela explicação sobre destructuring:
+  @Henrique Clementino - Turma 10 - Tribo B
+*/
+const renderProductsList = (productsList) => {
+  const items = document.querySelector('.items'); // obtém o elemento que possui a classe 'items'
+  productsList.forEach((product) => { // para cada produto na lista
+    // const { id, title, thumbnail } = product;
+    // const productCart = createProductItemElement({ sku: id, name: title, image: thumbnail });
+    const productCart = createProductItemElement(product); // cria um elemento HTML que o armazena
+    items.appendChild(productCart); // e o adiciona como filho de 'items'
+  });
+};
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -41,3 +86,8 @@ function createProductItemElement({ sku, name, image }) {
 //   li.addEventListener('click', cartItemClickListener);
 //   return li;
 // }
+
+window.onload = async function onload() {
+  // cria e renderiza a lista de produtos relacionados com uma consulta que contém o termo (palavra) passado como parâmetro
+  renderProductsList(await createProductsList());
+ };
