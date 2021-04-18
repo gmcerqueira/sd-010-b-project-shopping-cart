713104d1943ccd6f6527a105726531d9db1d35eb
Roger Franco <rogerfrancogalt@gmail.com>
2021-04-18 17:24:38 -0400

    variadas alterações

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// let total = 0;
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,12 +13,14 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ sku, name, image, price }) {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
+  const priceFixed = price.toFixed(2).replace('.', ',');
+  section.appendChild(createCustomElement('span', 'item__title', priceFixed));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
@@ -25,25 +28,27 @@ function createProductItemElement({ sku, name, image }) {
 }
 
 function storageCart() {
-  localStorage.clear('cart');
+  // localStorage.clear('cart');
   const myStorage = document.getElementsByClassName('cart__items')[0].innerHTML;
   console.log(myStorage);
   localStorage.setItem('cart', myStorage);
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-  event.currentTarget.classList.contains('clicked');
-    event.currentTarget.classList.add('clicked');
-  event.currentTarget.remove('clicked');
-  storageCart();
-  }
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui 
+//     // event.target.remove()
+//   console.log(event.currentTarget.classList.contains('clicked'));
+  // storageCart();
+//   }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', (e) => { 
+    e.target.remove(); 
+  storageCart();
+});
   return li;
 }
 
@@ -66,12 +71,12 @@ function addObject() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json()).then((data) => {
       data.results.forEach((item) => {
-        const computer = createProductItemElement({
+        document.querySelector('.items').appendChild(createProductItemElement({
           sku: item.id,
           name: item.title,
           image: item.thumbnail,
-        });
-        document.querySelector('.items').appendChild(computer);
+          price: item.price,
+        }));
       });
     })
     .then(() => {
@@ -87,7 +92,16 @@ window.onload = function onload() {
   if (localStorage.getItem('cart') !== undefined) {
     document.getElementsByClassName('cart__items')[0].innerHTML = localStorage.getItem('cart');
     const li = document.querySelectorAll('li');
-    li.forEach((list) => list.addEventListener('click', cartItemClickListener));
+    li.forEach((list) => list.addEventListener('click', (e) => { 
+      e.target.remove(); 
+    storageCart();
+  }));
   }
   addObject();
 };
+
+// async function getCountries() {
+//   const countries = await fetch('https://restcountries.eu/rest/v2/all').then((res) => res.json());
+//   countries.forEach((country) => {
+//     appendItemList(country.translations.br);
+//   });
