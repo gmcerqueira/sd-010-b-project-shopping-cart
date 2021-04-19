c626242fa9cee31b4fece037947a9add8d2d3e04
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-04-19 09:13:22 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
-// window.onload = function onload() { };
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  return section;
+}
 
-//   return section;
-// }
+// No requisito 1 fiquei um pouco perdido no que diz respeito a estruturação da resposta, mesmo tendo uma ideia de como fazê-la. Então busquei me orientar com o meu colega Leandro Reis, foi então que eu percebi o que o requisito realmente pedia. link: https://github.com/tryber/sd-010-b-project-shopping-cart/pull/6/commits/4313a30ae6b5617cd45bd6434e2e5c50e9144818.
+
+async function getProducts() {
+  const myFetch = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((response) => response.json())
+  .then((response) => response.results);
+  myFetch.forEach((product) => {
+    // Aqui acrescentamos os respectivos valores das chaves do destruction.
+    const createProduct = createProductItemElement({
+      sku: product.id,
+      name: product.title,
+      image: product.thumbnail,
+    });
+    // Aqui incorporamos o elemento HTML criado ao elemento pai(.items).
+    const elementFather = document.querySelector('.items');
+    elementFather.appendChild(createProduct);
+  });
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
 // function cartItemClickListener(event) {
-//   // coloque seu código aqui
+//   //
 // }
 
 // function createCartItemElement({ sku, name, salePrice }) {
@@ -41,3 +58,7 @@
 //   li.addEventListener('click', cartItemClickListener);
 //   return li;
 // }
+
+window.onload = function onload() { 
+  getProducts();
+};
