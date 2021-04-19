f28d848afb43c9487bf949d20e9e369e10a07ab9
Lara Capila <laracapilak@gmail.com>
2021-04-19 20:35:09 -0300

    E la vamos nos de novo

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const endpointComputer = 'https://api.mercadolibre.com/sites/MLB/search?q=';
 const endpointComputerItem = 'https://api.mercadolibre.com/items/';
+const chaveLocalStorage = 'carrinho';
 
 // Faz requisição para API buscando todos os computadores;
 const requestAPIComputer = async (produto) => {
@@ -17,8 +18,36 @@ const requestAPIItem = async (item) => {
   return data;
 };
 
-function cartItemClickListener(event) {
+// Remove item do carrinho do LocalStorage;
+const removeLocalStorage = async (item) => {
+  // variável pega carrinho do Local Storage
+  let carrinho = await JSON.parse(localStorage.getItem(chaveLocalStorage));
+  if (carrinho !== null) {
+    carrinho.forEach((element, index) => {
+      if (element === item) {
+        carrinho.splice(index, 1);
+      }
+    });
+  } else {
+    carrinho = [];
+  }
+  localStorage.setItem(chaveLocalStorage, JSON.stringify(carrinho));
+};
+
+// Subtrai item preço total;
+const subPrice = async (price) => {
+  const getSpanPrice = document.querySelector('.price');
+  getSpanPrice.innerText = (parseFloat(getSpanPrice.innerText) - price).toFixed(2);
+};
+
+// Remove item do carrinho ao ser clicado;
+async function cartItemClickListener(event) {
+  const { id } = event.target.dataset;
+  console.log(event);
   event.target.remove();
+  removeLocalStorage(id);
+  const item = await requestAPIItem(id);
+  subPrice(item.price);
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -44,12 +73,31 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Adiciona item do carrinho no local storage;
+const addLocalStorage = async (item) => {
+  let carrinho = await JSON.parse(localStorage.getItem(chaveLocalStorage));
+  if (carrinho !== null) {
+    carrinho.push(item);
+  } else {
+    carrinho = [item];
+  }
+  localStorage.setItem(chaveLocalStorage, JSON.stringify(carrinho));
+};
+
+// Soma preço total;
+const sumPrice = async (price) => {
+  const getSpanPrice = document.querySelector('.price');
+  getSpanPrice.innerText = (parseFloat(getSpanPrice.innerText) + price).toFixed(2);
+};
+
 // Adiciona item no carrinho ao clicar no botão 'Adicinar no carrinho';
 const addCard = async (event) => {
   const id = event.target.parentNode;
   const dataItem = await requestAPIItem(id.dataset.id);
   const getOl = document.querySelector('.cart__items');
   getOl.appendChild(createCartItemElement(dataItem));
+  await addLocalStorage(id.dataset.id);
+  sumPrice(dataItem.price);
 };
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
@@ -79,10 +127,43 @@ const createListItems = (data, section) => {
   });
 };
 
+// Os itens do carrinho permanacem ao carregar a página;
+const loadingCart = async () => {
+  // variável pega carrinho do Local Storage
+  const carrinho = await JSON.parse(localStorage.getItem(chaveLocalStorage));
+
+  if (carrinho !== null) {
+    carrinho.forEach(async (element) => {
+      const getOl = document.querySelector('.cart__items');
+      const item = await requestAPIItem(element);
+      getOl.appendChild(createCartItemElement(item));
+    });
+  }
+};
+
+// O preço total permanace ao carregar a página;
+// const loadingCartPrice = async () => {
+//   // variável pega carrinho do Local Storage
+//   const carrinho = await JSON.parse(localStorage.getItem(chaveLocalStorage));
+//   let valueTotal = 0;
+
+//   if (carrinho !== null) {
+
+//     for (let index = 0; index < carrinho.length; index += 1) {
+//       requestAPIItem(carrinho[index])
+//       .then((item) => {
+//         valueTotal += item.price;
+//       });     
+//     }
+//   }
+//   console.log(valueTotal);
+// };
+
 // Função principal
 window.onload = async function onload() {
   const data = await requestAPIComputer('computador');
-  // console.log('data ', data);
   const sectionItems = document.querySelector('.items');
   await createListItems(data, sectionItems);
+  await loadingCart();
+  // await loadingCartPrice();
  };
