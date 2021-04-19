dc992d33e3feabe08b2b565aa342211a6338e12d
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-04-18 21:12:48 -0300

    requisito 3

--- script.js
+++ script.js
@@ -40,22 +40,23 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  event.target.remove();
+  const ol = document.getElementsByClassName('cart__item');
+  console.log(ol);
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
 function renderComputer(computers) {
   const elements = document.getElementsByClassName('items')[0];
   const ol = document.getElementsByTagName('ol')[0];
-  console.log(ol);
   computers.results.forEach((its) => {
     const iten = createProductItemElement({ sku: its.id, name: its.title, image: its.thumbnail });
     elements.appendChild(iten);
