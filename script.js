b337fb1e714f77c2ef0621fb91d5bc0d89262444
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-04-19 23:30:48 -0300

    Iniciando requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -41,3 +39,21 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+function fetchItems() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+    .then((response) => response.json())
+    .then((items) => {
+      console.log(items.results[0]);
+      const newSection = createProductItemElement(items.results[0]);
+      const itemsSection = document.querySelector('.items');
+      itemsSection.appendChild(newSection);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+window.onload = () => {
+  fetchItems();
+};
