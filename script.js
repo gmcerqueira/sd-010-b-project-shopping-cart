24f4eb3f99d8afb9fa50fb6901973861dd6b74c2
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-04-19 21:45:11 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -47,18 +47,42 @@ async function getProducts() {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   //
-// }
+async function cartItemClickListener(event) {
+  //
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function createItemCart(event) {
+  const itemId = await event.target.id;
+  const getData = await fetch(`https://api.mercadolibre.com/items/${itemId}`)
+  .then(response => response.json());
+
+  getData.forEach(item => {
+    const createItemCart = createCartItemElement({
+      sku: item.id,
+      name: item.title,
+      salePrice: item.price,
+    })
+    const getFather = document.querySelector('.cart__items');
+    getFather.appendChild(createItemCart);
+  })
+}
+
+function getButtonAddItem() {
+  const getButton = document.querySelectorAll('.item__add');
+  getButton.forEach(button => button.addEventListener('click', createItemCart))
+}
 
 window.onload = function onload() { 
   getProducts();
+  getButtonAddItem();
 };
